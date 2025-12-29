1a8e48a995c0694783116f8ed0083fe5b614e331
cmkaranja <cmkaranja@safaricom.co.ke>
2025-12-29 20:07:09 +0300

    autowiring

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(basePackages = "beans")
